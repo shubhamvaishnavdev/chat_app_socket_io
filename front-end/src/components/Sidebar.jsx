1c21267c191876d997412434be8e74e083dc27eb
unknown <shubhamvaishnav.dev@gmail.com>
2023-08-20 12:50:49 +0530

    version 1.4

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -3,9 +3,9 @@ import { Contacts } from '../components'
 import { AiOutlineClose } from 'react-icons/ai';
 
 const Sidebar = ({ id, isOpen, setIsOpen, modal, setModal}) => {
-console.log("isOpen at sidebar " + isOpen);
+
     return (
-        <div className={`${isOpen ? 'sidebar absolute top-0 left-0 h-full z-20 w-[80%] block' : 'hidden'} 
+        <div className={`${isOpen ? 'sidebar fixed top-0 left-0 h-full z-20 w-[80%] block' : 'hidden'} 
         md:fixed md:block md:w-[25%] md:left-0 md:top-0 h-full  bg-gray-600 `}>
             <div className='h-[8%] flex items-center border-2 border-black'>
                 <p className='w-full py-2 text-center text-white font-semibold'
